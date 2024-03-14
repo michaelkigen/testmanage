6ac588e4ecafd46517aafc5c3e1619feda7160ca
michael <100795405+michaelkigen@users.noreply.github.com>
2024-03-14 16:19:12 +0300

    Update settings.py
    db

--- pro/settings.py
+++ pro/settings.py
@@ -98,13 +98,22 @@ WSGI_APPLICATION = 'pro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db33kb0d06oo83',
+        'USER': 'ua01an81ebs9ps',
+        'PASSWORD': 'p3e39d97bcd13ebc28d8b2156a509a21387ee9c09674352b5037655402b605fd5',
+        'HOST': 'ceu9lmqblp8t3q.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
